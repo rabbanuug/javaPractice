61b067a553669dcb283e8992ff5ee63737f0b8a0
Golam Rabbani <rabbanug@gmail.com>
2024-02-24 22:00:03 +0600

    file name changed

---
+++ Java-Classes-w3schools/Polymorphism/Main.java
@@ -0,0 +1,25 @@
+
+class Animal{
+    public void animalSound(){
+        System.out.println("Animal makes sound");
+    }
+
+}
+
+class Pig extends Animal {
+    public void animalSound() {
+        System.out.println("Pig says : Wee wee");
+    }
+}
+
+class Cat extends Animal{
+    public void animalSound(){
+        System.out.println("Cat says: meow");
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
